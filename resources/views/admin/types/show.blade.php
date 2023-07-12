517bda0e7d80378f1fe992305e25738fec8cfa3c
Alessandra-Casu <alessandracasu919@gmail.com>
2023-07-12 12:53:34 +0200

    corr types show

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -1,14 +1,14 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-
+   
     <h1>{{ $type->name }}</h1>
-    <p>{{ $type->description }}</p>
+    
 
     <h2>Type Project: </h2>
     <ul>
         @foreach ($type->projects as $project)
-            <li><a href="{{ route('admin.types.show', ['project' => $project]) }}">{{ $project->title }}</a></li>
+            <li><a href="{{ route('admin.projects.show', ['project' => $project]) }}">{{ $project->title }}</a></li>
         @endforeach
     </ul>
 
